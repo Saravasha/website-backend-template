b41a05c3411ba0da4619df0d8346bc344a2b827e
Saravasha <33827803+Saravasha@users.noreply.github.com>
2025-08-04 04:56:59 +0200

    update database

--- Migrations/20250804025606_init.Designer.cs
+++ Migrations/20250804025606_init.Designer.cs
@@ -12,7 +12,7 @@ using WebAppBackend.Data;
 namespace WebAppBackend.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20250804024149_init")]
+    [Migration("20250804025606_init")]
     partial class init
     {
         /// <inheritdoc />
