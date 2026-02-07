1c3396d4037aba0117baebc2fc133a6cadefdc23
Saravasha <33827803+Saravasha@users.noreply.github.com>
2026-02-07 03:12:30 +0100

    fixed some tokens

--- obj/Debug/net8.0/WebAppBackend.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebAppBackend.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAppBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+947dcb731bde27e0cc5c81f14d67164ec7444885")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00ddaf6fceb4a92f8e974b5f196e9aa362e636ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAppBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAppBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
